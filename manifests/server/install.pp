bab7e4b01cc36d20a194da179b39943acb49e161
Jussi Pöri <jussi.pori@yle.fi>
2014-08-01 13:19:37 +0300

    Added version handling to package

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,33 +1,10 @@
 # PRIVATE CLASS: do not call directly
 class mongodb::server::install {
-  $package_ensure = $mongodb::server::package_ensure
   $package_name   = $mongodb::server::package_name
-
-  case $package_ensure {
-    true:     {
-      $my_package_ensure = 'present'
-      $file_ensure     = 'directory'
-    }
-    false:    {
-      $my_package_ensure = 'absent'
-      $file_ensure     = 'absent'
-    }
-    'absent': {
-      $my_package_ensure = 'absent'
-      $file_ensure     = 'absent'
-    }
-    'purged': {
-      $my_package_ensure = 'purged'
-      $file_ensure     = 'absent'
-    }
-    default:  {
-      $my_package_ensure = $package_ensure
-      $file_ensure     = 'present'
-    }
-  }
+  $version        = $::mongodb::globals::version
 
   package { 'mongodb_server':
-    ensure  => $my_package_ensure,
+    ensure  => $version,
     name    => $package_name,
     tag     => 'mongodb',
   }
